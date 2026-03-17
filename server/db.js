bbb02bae237a609c52ce9d506a5a7dc041d3d2cd
saladin jake <juwavictor@gmail.com>
2026-03-17 06:15:53 -0700

    chore: add sqlide db linkup and run script

---
+++ server/db.js
@@ -0,0 +1,24 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+
+const dbPath = path.resolve(__dirname, 'marketplace.db');
+const db = new sqlite3.Database(dbPath);
+
+const run = (query, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.run(query, params, function (err) {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+};
+
+const all = (query, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.all(query, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+};
